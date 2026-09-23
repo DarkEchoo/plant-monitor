c66c322847475f9f53038cd1fb3086150d28e1ae
Tropic <78857605+DarkEchoo@users.noreply.github.com>
2026-09-22 21:32:55 -0600

    Add new plant statuses for analysis
    Added new plant statuses for better analysis.

--- include/analysis.h
+++ include/analysis.h
@@ -11,12 +11,12 @@ typedef struct {
 typedef enum {
     PLANT_STATUS_INVALID = -1,
     PLANT_STATUS_STABLE = 0,
-    PLANT_STATUS_DRYING,
-    PLANT_STATUS_WATER_SOON
+    PLANT_STATUS_WATER_SOON,
+    PLANT_STATUS_NOT_ENOUGH_LIGHT,
+    PLANT_STATUS_WATER_AND_LIGHT
 } plant_status;
 
 plant_status analyze_plant_snapshot(const plant_snapshot *snapshot);
 const char *plant_status_name(plant_status status);
 
 #endif
-
